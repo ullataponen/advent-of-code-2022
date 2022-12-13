3f0e74f9defaa777262a82504a49e8f46dbc6ea9
Ulla <ulla.taponen@gmail.com>
2022-12-13 21:56:08 +0200

    Rename utils->config

--- Day02-/index.ts
+++ Day02-/index.ts
@@ -1,5 +1,5 @@
 import { calculateRockPaperScissorsWinner } from "./02/app";
-import { getInputFromClient, submitAnswerToClient } from "./utils";
+import { getInputFromClient, submitAnswerToClient } from "./config";
 
 export const getInputForDay = async (): Promise<string> =>
   (await getInputFromClient()) as string;
